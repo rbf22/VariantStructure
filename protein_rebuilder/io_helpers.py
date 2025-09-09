3686eb2abc448f3bd1cbafd25feaa90f3a89cfd4
rbf22 <bryn.fenwick@gmail.com>
2025-09-08 20:53:09 -0400

    Create io_helpers.py

---
+++ protein_rebuilder/io_helpers.py
@@ -0,0 +1,16 @@
+# protein_rebuilder/io_helpers.py
+from Bio.PDB import PDBParser, PDBIO, Select
+import io
+
+def read_pdb_file(pdb_path_or_handle):
+    parser = PDBParser(QUIET=True)
+    structure = parser.get_structure('ref', pdb_path_or_handle)
+    return structure
+
+class _KeepAll(Select):
+    def accept_residue(self, residue): return True
+
+def write_pdb_file(structure, out_path):
+    io_writer = PDBIO()
+    io_writer.set_structure(structure)
+    io_writer.save(out_path, select=_KeepAll())
